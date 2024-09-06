0ea6b7c02911792b690be4a292fd08927828d5d7
yueb <zhaozhengyang_byn@pku.edu.cn>
2024-09-07 04:33:12 +0800

    feat: 优化代码结构

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
-/** the main file */
-
 import { createApp } from 'vue';
 import App from './App.vue';
 import { plugins } from './plugins';
 import '@/assets/styles/scss/_global-conf.scss';
 
-plugins.reduce((app, plugin) => app.use(plugin), createApp(App)).mount('#app');
+function main() {
+    return plugins.reduce((app, plugin) => app.use(plugin), createApp(App)).mount('#app');
+}
+
+main();
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,4 +1,2 @@
-// 全局常数
-
 /** global locale */
 export const globalLocale = 'zh-cn';
